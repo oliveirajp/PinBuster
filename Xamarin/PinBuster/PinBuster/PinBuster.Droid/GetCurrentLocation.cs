4106b2acab94ea528e6baa8003f28104041fa3c3
ZPaulo <jose.p.s.olive@gmail.com>
2016-04-28 10:49:52 +0100

    android location working

---
+++ Xamarin/PinBuster/PinBuster/PinBuster.Droid/GetCurrentLocation.cs
@@ -0,0 +1,85 @@
+using System;
+using Android.Content;
+using PinBuster.Droid;
+using Android.Locations;
+using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Util;
+
+[assembly: Xamarin.Forms.Dependency(typeof(GetCurrentLocation))]
+namespace PinBuster.Droid
+{
+    public class LocationEventArgs : EventArgs, ILocationEventArgs
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+
+    class GetCurrentLocation : Java.Lang.Object, CLocation, ILocationListener
+    {
+        LocationManager lm;
+        public void OnProviderDisabled(string provider) { }
+        public void OnProviderEnabled(string provider) { }
+        public void OnStatusChanged(string provider, Availability status, Android.OS.Bundle extras) { }
+        public void OnLocationChanged(Location location)
+        {
+            if (location != null)
+            {
+                LocationEventArgs args = new LocationEventArgs();
+                args.lat = location.Latitude;
+                args.lng = location.Longitude;
+                locationObtained(this, args);
+            };
+        }
+        
+        //---an EventHandler delegate that is called when a location is obtained
+        public event EventHandler<ILocationEventArgs>
+            locationObtained;
+
+        //---custom event accessor that is invoked when client subscribes to the event
+        event EventHandler<ILocationEventArgs> CLocation.locationObtained
+        {
+            add
+            {
+                locationObtained += value;
+            }
+            remove
+            {
+                locationObtained -= value;
+            }
+        }
+
+        //method to call to start getting location
+        public void IGetCurrentPosition()
+        {
+            string locationProvider;
+
+            lm = (LocationManager) Forms.Context.GetSystemService(Context.LocationService);
+
+            Criteria criteriaForLocationService = new Criteria
+            {
+                Accuracy = Accuracy.Fine
+            };
+            IList<string> acceptableLocationProviders = lm.GetProviders(criteriaForLocationService, true);
+
+            if (acceptableLocationProviders.Any())
+            {
+                locationProvider = acceptableLocationProviders.First();
+            }
+            else
+            {
+                locationProvider = string.Empty;
+            }
+            Log.Debug(null, "Using " + locationProvider + ".");
+
+            lm.RequestLocationUpdates(locationProvider, 2000, 1, this);
+        }
+       
+        ~GetCurrentLocation()
+        {
+            lm.RemoveUpdates(this);
+        }
+    }
+}
--- Xamarin/PinBuster/PinBuster/PinBuster/App.cs
+++ Xamarin/PinBuster/PinBuster/PinBuster/App.cs
@@ -10,26 +10,40 @@ using PinBuster;
 namespace PinBuster
 {
     public class App : Application
-	{
-		private readonly static Locator _locator  = new Locator();
+    {
 
-		public static PinsManager PinsManager { get; private set; }
+        CLocation loc;
 
-		public static Locator Locator
-		{
-			get { return _locator; }
-		}
+        private readonly static Locator _locator = new Locator();
+
+        public static PinsManager PinsManager { get; private set; }
+
+        public static Locator Locator
+        {
+            get { return _locator; }
+        }
 
         public App()
         {
+
+
             // The root page of your application
-			PinsManager = new PinsManager();
-			MainPage = new MapPage();
+            PinsManager = new PinsManager();
+            MainPage = new MapPage();
         }
 
         protected override void OnStart()
         {
             // Handle when your app starts
+            loc = DependencyService.Get<CLocation>();
+            loc.locationObtained += (object sender,
+                ILocationEventArgs e) =>
+            {
+                var lat = e.lat;
+                var lng = e.lng;
+                System.Diagnostics.Debug.WriteLine("Lat: " + lat + " Lon: " + lng);
+            };
+            loc.IGetCurrentPosition();
         }
 
         protected override void OnSleep()
@@ -39,7 +53,7 @@ namespace PinBuster
 
         protected override void OnResume()
         {
-            // Handle when your app resumes
+            System.Diagnostics.Debug.WriteLine("teste1");
         }
     }
 }
