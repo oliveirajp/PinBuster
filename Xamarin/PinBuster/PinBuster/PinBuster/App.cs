9b82e41028ae7a4e4af58a2431e69a9ac0e5b9e2
Filipe Batista <filipedgb@gmail.com>
2016-04-29 18:34:39 +0200

    Post with dummy values

--- Xamarin/PinBuster/PinBuster/PinBuster/App.cs
+++ Xamarin/PinBuster/PinBuster/PinBuster/App.cs
@@ -11,20 +11,7 @@ namespace PinBuster
     {
         public App()
         {
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new post();
         }
 
         protected override void OnStart()
---
+++ Xamarin/PinBuster/PinBuster/PinBuster/post.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace PinBuster
+{
+    public partial class post : ContentPage
+    {
+        public post()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+
+            var postData = new List<KeyValuePair<string, string>>();
+
+            postData.Add(new KeyValuePair<string, string>("latitude", "0"));
+            postData.Add(new KeyValuePair<string, string>("longitude", "0"));
+            postData.Add(new KeyValuePair<string, string>("data", "20130210 11:11:11 PM"));
+            postData.Add(new KeyValuePair<string, string>("tempo_limite", "30"));
+            postData.Add(new KeyValuePair<string, string>("raio", "50"));
+            postData.Add(new KeyValuePair<string, string>("utilizador_id", "0"));
+            postData.Add(new KeyValuePair<string, string>("conteudo", PostMessage.Text));
+            postData.Add(new KeyValuePair<string, string>("localizacao", "leiden"));
+            postData.Add(new KeyValuePair<string, string>("categoria","xD"));
+
+            using (var client = new System.Net.Http.HttpClient())
+            {
+                client.BaseAddress = new Uri("http://pinbusterapitest.azurewebsites.net");
+                var content = new System.Net.Http.FormUrlEncodedContent(postData);
+
+
+                var result = client.PostAsync("api/mensagem", content).Result;
+                string resultContent = result.Content.ReadAsStringAsync().Result;
+
+                outputpost.Text = resultContent;
+            }
+
+            PostMessage.Text = String.Empty;
+        }
+    }
+
+}
+
