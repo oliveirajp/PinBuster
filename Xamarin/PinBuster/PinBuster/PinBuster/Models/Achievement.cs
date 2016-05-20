71e735256e3b5de614a8361e6d72759dddb218a1
jessicaNamora <jessicaNamora@users.noreply.github.com>
2016-05-20 17:51:51 +0100

    Add files via upload

---
+++ Xamarin/PinBuster/PinBuster/PinBuster/Models/Achievement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PinBuster.Models
+{
+    class Achievement
+    {
+        public int face_id { get; set; }
+        public String nome { get; set; }
+        public int MessagesNeeded { get; set; }
+        public int MessagesFound { get; set; }
+
+        public Achievement(int face_id, String nome, int MessagesNeeded, int MessagesFound)
+        {
+            this.face_id = face_id;
+            this.nome = nome;
+            this.MessagesNeeded = MessagesNeeded;
+            this.MessagesFound = MessagesFound;
+        }
+    }
+}
