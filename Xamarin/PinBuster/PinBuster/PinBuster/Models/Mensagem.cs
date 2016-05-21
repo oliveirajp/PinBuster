9489b2c57065d0653dbbe7de888d6e695fce7a84
ZPaulo <jose.p.s.olive@gmail.com>
2016-05-21 15:54:18 +0100

    Merge remote-tracking branch 'origin/sprint3' into sprint3
    # Conflicts:
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/AppX/entrypoint/PinBuster.UWP.exe
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/AppX/resources.pri
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/AppX/vs.appxrecipe
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/PinBuster.UWP.exe
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/PinBuster.UWP.pdb
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/PinBuster.pdb
    
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/XamarinMapOverlay.xbf
    #	Xamarin/PinBuster/PinBuster/PinBuster.UWP/bin/x86/Debug/resources.pri
    #	Xamarin/PinBuster/PinBuster/PinBuster/bin/Debug/PinBuster.pdb

---
+++ Xamarin/PinBuster/PinBuster/PinBuster/Models/Mensagem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PinBuster.Models
+{
+    class Mensagem
+    {
+        public int mensagem_id { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public String data { get; set; }
+        public int tempo_limite { get; set; }
+        public int raio { get; set; }
+        public int face_id { get; set; }
+        public String conteudo { get; set;  }
+        public String localizacao { get; set; }
+        public String categoria { get; set;  }
+
+        public Mensagem(int mensagem_id, double latitude, double longitude, String data, int tempo_limite, int raio, int face_id, String conteudo, String localizacao, String categoria)
+        {
+            this.mensagem_id = mensagem_id;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.data = data;
+            this.tempo_limite = tempo_limite;
+            this.raio = raio;
+            this.face_id = face_id;
+            this.conteudo = conteudo;
+            this.localizacao = localizacao;
+            this.categoria = categoria;
+        }
+    }
+}
